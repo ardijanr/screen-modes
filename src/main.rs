c3c9d4e7b74f90d2b401bbe596935c4c042f2f49
ardijanr <a.rexhaj@stud.uis.no>
2021-01-07 21:44:12 +0100

    added comments, maybe to many

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::env;
 use packer::Packer;
 use std::{io::Bytes, process::Command, vec};
 
+
 struct Monitor {
     name: String,
     // enabled: bool,
@@ -13,67 +14,105 @@ struct Monitor {
     primary: bool,
 }
 
-fn check_active_monitors() -> Vec<Monitor> {
-    let xrandr = Command::new("xrandr").output().expect("some error");
 
+//
+// ─── XRANDR OUTPUT PARSER START ────────────────────────────────────────────────────────
+//
+
+fn check_active_monitors() -> Vec<Monitor> {
+    //run the command and capture output
+    let xrandr = Command::new("xrandr").output().expect("Could not run command");
+
+    //Convert to string
     let input_string = String::from_utf8(xrandr.stdout).unwrap();
+
+    //Split to lines
     let mut lines = input_string.lines();
+
+    //Create the vector where we will store the monitors found
     let mut monitor_vec: Vec<Monitor> = Vec::new();
 
-    while let Some(i) = lines.next() {
-        let mut words = i.split_whitespace();
-        let name = words.next();
-        // let mut screen_enabled = false;
-        let mut primary = false;
+    //Pattern-matching for each line, unwrapping option to get the value.
+    while let Some(single_line) = lines.next() {
 
+        //Split lines again into "words" which will be used alot
+        let mut words = single_line.split_whitespace();
+
+        //First word is the name of the monitor
+        let monitor_name = words.next();
+
+        //Code that might come in handy, but commented out for now
+        // let mut screen_enabled = false;
+
+        //Create default variable for is this primary.
+        let mut primary=false;
+
+        //Check if the second word says connected
         if words.next() == Some("connected") {
+            //Check if its primary
             if words.next() == Some("primary") {
                 primary = true;
             }
 
-            //if the moitor is connected and contains * we need to save that variable
+            //Create a vector to store the resolutions that are supported
             let mut resolutions_vec: Vec<String> = Vec::new();
-            let mut first_res_line = lines.next().unwrap().split_whitespace();
-            resolutions_vec.push(String::from(first_res_line.next().unwrap()));
 
+            //This is the first monitor resolution. Also the highest resolution that monitor supports according to xrandr
+            resolutions_vec.push(String::from(lines.next().unwrap().split_whitespace().next().unwrap()));
+
+            //Code that might come in handy, but commented out for now
+            //if the monitor is connected and contains * it means that is currently displaying on that mode
             //checks the entire line
             // if first_res_line.next().unwrap().contains(&"*"){
             //     screen_enabled = true;
             // };
 
-            while let Some(resolution_line) = lines.next() {
-                let resolution = resolution_line.split_whitespace().next().unwrap();
+            //The next few lines are resolutions the monitor supports
+            //They are iterated until we get to a resolution that is to low to matter
+            while let Some(resolution) = lines.next().unwrap().split_whitespace().next() {
 
                 let left_side = String::from(resolution.split("x").next().unwrap());
-                // println!("{}",left_side);
-                if left_side.parse::<i32>().unwrap() > 1000 {
-                    resolutions_vec.push(String::from(resolution))
-                } else {
+
+                //Check if the horizontal resolution is to low to matter and break out of while if it is
+                if left_side.parse::<i32>().unwrap() < 1000 {
                     break;
                 }
+
+                //Otherwise save the resolution
+                resolutions_vec.push(String::from(resolution))
             }
 
+            //Add the monitor that we found to a vector which is returned when while loop finishes.
             monitor_vec.push(Monitor {
-                name: String::from(name.unwrap()),
-                // enabled: screen_enabled,
+                name: String::from(monitor_name.unwrap()),
                 resolutions: resolutions_vec,
                 primary: primary,
             })
         }
     }
+//
+// ─── XRANDR OUTPUT PARSER END ────────────────────────────────────────────────────
+//
+
+
+
     //Check if you don't have any other monitors connected, enable that one and quit.
     if monitor_vec.len() == 1 {
         Command::new("xrandr")
             .args(&["--output", &(monitor_vec[0].name), "--auto"])
             .output()
-            .expect("some error");
+            .expect("Could not run command");
 
-        //Temporary solution, will fix once iced supports closing the window
         std::process::exit(0)
     }
     return monitor_vec;
 }
 
+
+//Resolution finder, starts checking from the primary displays highest resolution
+//The order highest to lowest, is due to the order of the xrandr output
+//
+//Takes the primary display in the first input, input order matters.
 fn find_common_res(primary: Vec<String>, secondary: Vec<String>) -> (usize, usize) {
     for i in 0..primary.len() {
         for j in 0..secondary.len() {
@@ -82,15 +121,15 @@ fn find_common_res(primary: Vec<String>, secondary: Vec<String>) -> (usize, usiz
             }
         }
     }
-
     return (0, 0);
 }
 
-//Currently only works with one external monitor.
+//Currently only works with one external monitor, and one primary monitor
 pub fn set_mode(message: Message) {
     let mut active_monitors = check_active_monitors();
     let mut primary_index = 0;
 
+    //Find the index of the primary display
     for i in 0..active_monitors.len() {
         if active_monitors[i].primary == true {
             primary_index = i
@@ -100,6 +139,7 @@ pub fn set_mode(message: Message) {
     //primary monitor is not the currently active one, its what is set as primary in xrandr
     let primary_monitor = active_monitors.remove(primary_index);
 
+    //Match what button was pressed, run command and close
     match message {
         Message::ModePrim => {
             Command::new("xrandr")
@@ -112,9 +152,8 @@ pub fn set_mode(message: Message) {
                     "--off",
                 ])
                 .output()
-                .expect("some error");
+                .expect("Could not run command");
 
-            //Temporary solution, will fix once iced supports closing the window
             std::process::exit(0)
         }
 
@@ -129,9 +168,8 @@ pub fn set_mode(message: Message) {
                     "--auto",
                 ])
                 .output()
-                .expect("some error");
+                .expect("Could not run command");
 
-            //Temporary solution, will fix once iced supports closing the window
             std::process::exit(0)
         }
 
@@ -155,12 +193,12 @@ pub fn set_mode(message: Message) {
                     &(primary_monitor.name),
                 ])
                 .output()
-                .expect("some error");
+                .expect("Could not run command");
 
-            //Temporary solution, will fix once iced supports closing the window
             std::process::exit(0)
         }
 
+        //Set mode to extended, defaults to left because mine is on my left.
         Message::ModeExt => {
             Command::new("xrandr")
                 .args(&[
@@ -174,23 +212,29 @@ pub fn set_mode(message: Message) {
                     &(primary_monitor.name),
                 ])
                 .output()
-                .expect("some error");
+                .expect("Could not run command");
 
-            //Temporary solution, will fix once iced supports closing the window
+
             std::process::exit(0)
         }
     }
 }
+
+//Packer struct for storing assets in binary
 #[derive(Packer)]
 #[packer(source = "assets")]
 struct Assets;
 
+//Creates a handle struct for loading svg data from binary,
+//Luckily iced implements a way to load svg's from bytes, just needs a little coercion.
 fn svg_create_handle(file_name: &str) -> svg::Handle {
     let data: Option<&'static [u8]> = Assets::get(file_name);
-    if data==None{
-        return svg::Handle::from_memory(Vec::new())
+    match data{
+        None => {return svg::Handle::from_memory(Vec::new())
+        }
+        Some(data) => {return svg::Handle::from_memory(data);
+        }
     }
-    return svg::Handle::from_memory(data.unwrap());
 }
 
 // THIS MAIN IS FOR TESTING
@@ -206,9 +250,9 @@ fn svg_create_handle(file_name: &str) -> svg::Handle {
 fn main() -> iced::Result {
     // This is run to check if any monitors we only have one monitor connected,
     // if so it will enable the connected monitor and immediately close
-    // println!("{:?}", Assets::list());
-
     check_active_monitors();
+
+    //setting resizable:false, and max_size seems to force floating on tiling window managers
     let settings = Settings {
         window: window::Settings {
             max_size: Some((400, 450)),
@@ -228,6 +272,7 @@ struct ScreenMode {
     image_4: button::State,
 }
 
+//message struct for button presses
 #[derive(Debug, Clone, Copy)]
 pub enum Message {
     ModePrim,
@@ -236,38 +281,35 @@ pub enum Message {
     ModeExt,
 }
 
+// iced sandbox
 impl Sandbox for ScreenMode {
     type Message = Message;
 
+    //iced
     fn new() -> Self {
         Self::default()
     }
 
+    //Window title
     fn title(&self) -> String {
         String::from("Screen Mode Selector")
     }
 
+
+    //Usually match statement happens here, but for future implementation of
+    //selection with keyboard another match statement is needed,
+    //therefore actual commands are in their own function.
     fn update(&mut self, message: Message) {
         set_mode(message)
     }
 
     fn view(&mut self) -> Element<Message> {
-        // let current_dir: String = env::current_dir()
-        //     .unwrap()
-        //     .as_os_str()
-        //     .to_str()
-        //     .unwrap()
-        //     .to_string();
-        // let path_prim: String = format!("{}/assets/primary-only.svg", current_dir); //probably causing issues
-        // let path_seco: String = format!("{}/assets/secondairy-only.svg", current_dir);
-        // let path_dup: String = format!("{}/assets/duplicate.svg", current_dir);
-        // let path_ext: String = format!("{}/assets/extended.svg", current_dir);
 
-        // println!("{}", path_prim);
-
+        //iced event handling, content is put in a column
         let content = Column::new()
             .padding(20)
             .align_items(Align::Center)
+            //Pushing buttons into column
             .push(
                 Button::new(
                     &mut self.image_1,
@@ -301,6 +343,7 @@ impl Sandbox for ScreenMode {
                 .style(style::Button::Primary),
             );
 
+        //A container is needed to set background color
         Container::new(content)
             .width(Length::Fill)
             .height(Length::Fill)
@@ -311,6 +354,7 @@ impl Sandbox for ScreenMode {
     }
 }
 
+//Styling of background and buttons
 mod style {
     use iced::{button, container, Background, Color, Vector};
 
@@ -330,7 +374,7 @@ mod style {
                     30.0 / 255.0,
                     36.0 / 255.0,
                     41.0 / 255.0,
-                ))), //30, 36, 41
+                ))),
                 ..container::Style::default()
             }
         }
@@ -340,7 +384,7 @@ mod style {
         fn active(&self) -> button::Style {
             button::Style {
                 background: Some(Background::Color(match self {
-                    Button::Primary => Color::from_rgb(30.0 / 255.0, 36.0 / 255.0, 41.0 / 255.0), //15, 87, 148
+                    Button::Primary => Color::from_rgb(30.0 / 255.0, 36.0 / 255.0, 41.0 / 255.0),
                     Button::Selected => Color::from_rgb(15.0 / 255.0, 87.0 / 255.0, 148.0 / 255.0),
                 })),
                 border_radius: 12.0,
